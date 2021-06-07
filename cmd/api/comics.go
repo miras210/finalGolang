629aa19a94eed53848121202f57af8764f192821
Miras <sektor.miras@gmail.com>
2021-06-07 17:54:24 +0600

    added: json decoding

--- cmd/api/comics.go
+++ cmd/api/comics.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/miras210/finalGolang/internal/data"
 	"net/http"
@@ -10,7 +11,20 @@ import (
 // Add a createComicsHandler for the "POST /v1/comics" endpoint. For now we simply
 // return a plain-text placeholder response.
 func (app *application) createComicsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new comic book")
+
+	var input struct {
+		Title string `json:"title"`
+		Year  int32  `json:"year"`
+		Pages int32  `json:"runtime"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showComicsHandler(w http.ResponseWriter, r *http.Request) {

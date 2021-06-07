7dc0c6ce94bc708cc19c3a19facdfcaf8657042f
Miras <sektor.miras@gmail.com>
2021-06-07 16:56:57 +0600

    added: router

--- cmd/api/healtcheck.go
+++ cmd/api/healtcheck.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// Declare a handler which writes a plain-text response with information about the
+// application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/miras210/finalGolang
 
 go 1.16
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect

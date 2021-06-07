4f2d65e883a7c81e49dd49883298671e55c6f2f8
Miras <sektor.miras@gmail.com>
2021-06-07 17:24:28 +0600

    added: comics data struct

--- cmd/api/comics.go
+++ cmd/api/comics.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/miras210/finalGolang/internal/data"
 	"net/http"
+	"time"
 )
 
 // Add a createComicsHandler for the "POST /v1/comics" endpoint. For now we simply
@@ -22,5 +24,20 @@ func (app *application) showComicsHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of comics %d\n", id)
+	// Create a new instance of the Comics struct, containing the ID we extracted from
+	// the URL and some dummy data. Also notice that we deliberately haven't set a
+	// value for the Year field.
+	comics := data.Comics{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Batman",
+		Pages:     165,
+		Version:   1,
+	}
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJSON(w, http.StatusOK, comics, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
---
+++ internal/data/comics.go
@@ -0,0 +1,14 @@
+package data
+
+import (
+	"time"
+)
+
+type Comics struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Pages     int32     `json:"pages,omitempty"`
+	Version   int32     `json:"version"`
+}

ebcbe0140c2a6bc325f6522e4a245584b981af5f
Miras <sektor.miras@gmail.com>
2021-06-20 20:31:01 +0600

    added: Requiring User Activation

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,11 +13,12 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/comics", app.createComicsHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/comics", app.listComicsHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/comics/:id", app.showComicsHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/comics/:id", app.updateComicsHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/comics/:id", app.deleteComicsHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/comics", app.requireActivatedUser(app.createComicsHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/comics", app.requireActivatedUser(app.listComicsHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/comics/:id", app.requireActivatedUser(app.showComicsHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/comics/:id", app.requireActivatedUser(app.updateComicsHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/comics/:id", app.requireActivatedUser(app.deleteComicsHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
